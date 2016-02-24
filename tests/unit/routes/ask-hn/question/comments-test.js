40365b840a208b79a9245bfd8caede07e5f8480f
Steve Chikwaya <steve_chikwaya@gallup.com>
2016-02-23 21:59:18 -0600

    Initial scaffold

---
+++ tests/unit/routes/ask-hn/question/comments-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:ask-hn/question/comments', 'Unit | Route | ask hn/question/comments', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
